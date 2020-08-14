9a3159a49ed3305c3337c0a3f53cb4e72c9e3fe0
easrng <easrng@gmail.com>
2020-08-14 19:17:36 -0400

    Bugfix

--- b-i-f.user.js
+++ b-i-f.user.js
@@ -1,31 +1,30 @@
 // ==UserScript==
 // @name        Broken Image Fixer
 // @namespace   https://easrng.github.io/
-// @match       https://scratch.mit.edu/discuss/*
+// @match       https://scratch.mit.edu/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      A-E-
-// @homepageURL https://github.com/easrng/broken-image-fixer
-// @supportURL  https://github.com/easrng/broken-image-fixer/issues
-// @updateURL   https://easrng.github.io/broken-image-fixer/b-i-f.user.js
+// @homepageURL https://github.com/easrng/fix-broken-images
+// @supportURL  https://github.com/easrng/fix-broken-images/issues
+// @updateURL   https://easrng.github.io/fix-broken-images/f-b-i.user.js
 // @description Fixes the broken images resulting from the Wikimedia Open Redirect fix, plus tries to fix some Tinypic images.
+// @run-at      document-end
 // ==/UserScript==
 
 function goodbye(){
-  this.src=https://tinypic.com/images/goodbye.jpg
+  this.src="https://tinypic.com/images/goodbye.jpg"
 }
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('Fixing broken images...');
-    for(let i of document.querySelectorAll('[src*="://secure.wikimedia.org/wikipedia/"][src*="%5c/.."]')){
-        let m=i.src.match(/^https?:\/\/secure.wikimedia.org\/wikipedia\/([^\/]+)%5c\/..%5c(.+)/i);
-        i.src=`https://${m[1]}/${m[2]}`;
+console.log('Fixing broken images...');
+for(let i of document.querySelectorAll('[src*="://secure.wikimedia.org/wikipedia/"]')){
+  let m=i.src.match(/^https?:\/\/secure.wikimedia.org\/wikipedia\/([^\/]+)%5c\/..%5c(.+)/i);
+    i.src=`https://${m[1]}/${m[2]}`;
+  }
+  for(let i of document.querySelectorAll('[src*=".tinypic.com/"]')){
+    let m=i.src.match(/^https?:\/\/i[^\.]+\.tinypic.com\/[^\/]+\..+$/i);
+    if(m){
+      i.src=`https://web.archive.org/web/1im_/${i.src}`;
+      i.addEventListener("error",goodbye)
     }
-    for(let i of document.querySelectorAll('[src*=".tinypic.com/"]')){
-        let m=i.src.match(/^https?:\/\/i[^\.]+\.tinypic.com\/[^\/]+\..+$/i);
-        if(m){
-            i.src=`https://web.archive.org/web/1im_/${i.src}`;
-            i.addEventListener("error",goodbye)
-        }
-    }
-});
+}
